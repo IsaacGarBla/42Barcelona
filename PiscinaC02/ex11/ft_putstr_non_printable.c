56b31aad301991a451cf000b9d20dc89c60395b5
Isaac A. García <IsaacGarBla@gmail.com>
2026-03-15 01:32:00 +0100

    Noche

--- PiscinaC02/ex11/ft_putstr_non_printable.c
+++ PiscinaC02/ex11/ft_putstr_non_printable.c
@@ -6,24 +6,24 @@
 /*   By: igarcia- <igarcia- <marvin@42.fr>  >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/11 17:42:56 by igarcia-          #+#    #+#             */
-/*   Updated: 2026/03/12 01:29:07 by igarcia-         ###   ########.fr       */
+/*   Updated: 2026/03/15 01:29:44 by igarcia-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_char_hex(int valor)
+void	ft_print_char_hex(char c)
 {
 	char	*dig_hex;
 
 	dig_hex = "0123456789abcdef";
-	write(1, &dig_hex[valor / 16], 1);
-	write(1, &dig_hex[valor % 16], 1);
+	write(1, &dig_hex[c / 16], 1);
+	write(1, &dig_hex[c % 16], 1);
 }
 
-int	ft_char_is_printable(char c)
+int	ft_char_is_printable(unsigned char c)
 {
-	if (c <= 31 || c == 127)
+	if (c <= 31 || c >= 127)
 		return (0);
 	else
 		return (1);
@@ -46,9 +46,3 @@ void	ft_putstr_non_printable(char *str)
 		x++;
 	}
 }
-
-/*int	main(void)
-{
-	ft_putstr_non_printable("Coucou\ntu vas bien ?");
-	return (0);
-}*/
