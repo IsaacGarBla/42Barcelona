3f5ea48a7fa98ab161c992a2e9eb4c0e2c6ac54e
Isaac Antonio Garcia Blanco <igarcia-@car10s5.42barcelona.com>
2026-06-05 21:04:13 +0200

    T

---
+++ Test/ft_eliminate_list.c
@@ -0,0 +1,99 @@
+#include "ft_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void ft_eliminate_list(t_list **begin_list, void *data, int (*cmp)())
+{
+	t_list	*current;
+	t_list	*previous;
+	
+	previous = *begin_list;
+	current = *begin_list;
+	while (current != NULL)
+	{
+		if ((*cmp)(data, current->data))
+		{
+			if (current == previous)
+			{
+				*begin_list = current->next;
+				previous = current->next;
+				//free(current->data);
+				free(current);
+				current = previous;
+			}
+			else
+			{
+				previous->next = current->next;
+				//free(current->data);
+				free(current);
+				current = previous->next;
+			}
+		}
+		else
+		{
+			previous = current;
+			current = current->next;
+		}
+	}
+}
+
+
+
+
+int compare(void *data1, void *data2)
+{
+	return (data1 == data2);
+}
+
+
+void ft_print_lista(t_list **begin_list)
+{
+	t_list *current;
+	
+	current = *begin_list;
+	while (current != NULL)
+	{
+		printf("El valor del nodo es: %s\n", (char *) current->data);
+		current = current->next;
+	}
+}
+
+#include <stdio.h>
+
+int main()
+{
+	t_list	*lista;
+	char	*h = "Hola";
+	char	*m = "Mundo";
+	char	*f = "Fantastico";
+	
+	lista = malloc(sizeof(t_list));
+	lista->data = NULL;
+	lista->next = malloc(sizeof(t_list));
+	lista->next->data = h;
+	lista->next->next = malloc(sizeof(t_list));
+	lista->next->next-> data = m;	
+	lista->next->next->next = malloc(sizeof(t_list));
+	lista->next->next->next->data = f;	
+	lista->next->next->next->next = NULL;
+	
+	ft_print_lista(&lista);
+	printf("Paso 1.\n");	
+	ft_eliminate_list(&lista, m, &compare);
+	ft_print_lista(&lista);
+	printf("Paso 2.\n");	
+	ft_eliminate_list(&lista, NULL, &compare);
+	ft_print_lista(&lista);
+	printf("Paso 3.\n");	
+	ft_eliminate_list(&lista, f, &compare);
+	ft_print_lista(&lista);
+	printf("Paso 4.\n");	
+	ft_eliminate_list(&lista, h, &compare);
+	ft_print_lista(&lista);
+	printf("Paso 5.\n");	
+	ft_eliminate_list(&lista, h, &compare);
+	ft_print_lista(&lista);
+
+	return 1;
+}
+
